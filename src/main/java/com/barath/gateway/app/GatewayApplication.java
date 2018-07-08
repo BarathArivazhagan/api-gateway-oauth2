2801dc87a39fdead970fb8b4158c8ab424b2e729
Barath Arivazhagan <barathmacdec91@gmail.com>
2018-07-08 16:18:09 +0530

    initial commit

---
+++ src/main/java/com/barath/gateway/app/GatewayApplication.java
@@ -0,0 +1,22 @@
+package com.barath.gateway.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@SpringBootApplication
+@RestController
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String email(Principal principal) {
+        return "Hello " + principal.getName();
+    }
+}
